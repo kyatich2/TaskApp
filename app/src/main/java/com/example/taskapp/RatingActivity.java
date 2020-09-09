d74790ec020e7df566a4dfcaae9b082c0f9d29d7
iankiim98 <iankiim98@users.noreply.github.com>
2020-09-09 13:25:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/RatingActivity.java
@@ -0,0 +1,31 @@
+package com.example.taskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class RatingActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        ratingBar = findViewById(R.id.rating_bar);
+        button = findViewById(R.id.rating_submit);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(),s+"star", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
